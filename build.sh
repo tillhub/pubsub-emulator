baa11fd950086fe2620a116fab1e5fdda21d39a6
Robert Lindstaedt <robert.lindstaedt@gmail.com>
2018-09-17 14:24:15 +0200

    adds quay key

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ docker build -t pubsub-emulator:latest .
 docker tag pubsub-emulator:latest quya.io/tillhub/pubsub-emulator:latest
 docker tag pubsub-emulator:latest quya.io/tillhub/pubsub-emulator:$TAG
 docker tag pubsub-emulator:latest tillhub/pubsub-emulator:$TAG
+
+docker login -u="tillhub+tillhub_oss" -p="$QUAY_KEY" quay.io
 docker push quya.io/tillhub/pubsub-emulator:latest && docker push quya.io/tillhub/pubsub-emulator:$TAG

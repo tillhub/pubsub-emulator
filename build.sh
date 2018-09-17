fdbf14c8375b5b8ad2a36a26fcd7f5f16a8969cf
Robert Lindstaedt <robert.lindstaedt@gmail.com>
2018-09-17 14:28:28 +0200

    fix: fixes ref quay host

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export TAG=$(git describe --tags)
 docker build -t pubsub-emulator:latest .
-docker tag pubsub-emulator:latest quya.io/tillhub/pubsub-emulator:latest
-docker tag pubsub-emulator:latest quya.io/tillhub/pubsub-emulator:$TAG
+docker tag pubsub-emulator:latest quay.io/tillhub/pubsub-emulator:latest
+docker tag pubsub-emulator:latest quay.io/tillhub/pubsub-emulator:$TAG
 docker tag pubsub-emulator:latest tillhub/pubsub-emulator:$TAG
 
-docker login -u="tillhub+tillhub_oss" -p="$QUAY_KEY" quay.io
-docker push quya.io/tillhub/pubsub-emulator:latest && docker push quya.io/tillhub/pubsub-emulator:$TAG
+docker login -u="$QUAY_USER" -p="$QUAY_KEY" quay.io
+docker push quay.io/tillhub/pubsub-emulator:latest && docker push quay.io/tillhub/pubsub-emulator:$TAG
